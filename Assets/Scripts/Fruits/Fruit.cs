96f70a3d01e7e25a71e8eb53a096572f1528d1f3
karakilicustasi <yildirim.dasdemir@ceng.deu.edu.tr>
2021-12-27 07:32:23 +0300

    Initial setup has been implemented (Fruit controller, fruitScriptable and wall container)

---
+++ Assets/Scripts/Fruits/Fruit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit
+{
+    private string type;
+    private bool playable;
+    private float speed;
+
+    public Fruit(string type,float speed,bool playable)
+    {
+        this.type = type;
+        this.playable = playable;
+        this.speed = speed;
+    }
+
+    public bool isPlayable()
+    {
+        return playable;
+    }
+
+    public float getSpeed()
+    {
+        return speed;
+    }
+
+    public string getType()
+    {
+        return type;
+    }
+}
